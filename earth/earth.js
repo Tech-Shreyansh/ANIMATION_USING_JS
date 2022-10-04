e609834a518422dc8c880a98ca305e0f4815dbc7
tech-shreyansh <Shreyanshagarwal.16.2022@gmail.com>
2022-10-05 03:36:33 +0530

    added car and aeroplane

--- earth/earth.js
+++ earth/earth.js
@@ -1,9 +1,16 @@
 setInterval(() => {
     man()
 }, 100)
-let l = 0,
+setInterval(() => {
+    car()
+}, 35)
+setInterval(() => {
+    aeroplane()
+}, 35)
+
+let c = 0,
     m = 0,
-    n = 0;
+    a = 0;
 
 function man() {
     var man = document.getElementsByClassName('man');
@@ -12,4 +19,21 @@ function man() {
     man[0].style.top = 350 + Math.floor(m * 0.2) + 'px';
     if (m > 300)
         m = 0;
+}
+
+
+function car() {
+    var car = document.getElementsByClassName('car');
+    c += 5;
+    car[0].style.left = 250 + Math.floor(c) + 'px';
+    if (c > 800)
+        c = 0;
+}
+
+function aeroplane() {
+    var aeroplane = document.getElementsByClassName('aeroplane');
+    a += 5;
+    aeroplane[0].style.left = 250 + Math.floor(a) + 'px';
+    if (a > 800)
+        a = 0;
 }
